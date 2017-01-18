7e20d0193229d3f6b91526ffef4ffba06aae90a2
mr_el <elanebritton96@gmail.com>
2017-01-17 19:42:15 -0500

    added in onclick listener

--- app/src/main/java/com/mrel/refreshcafe/SpecialsMenu.java
+++ app/src/main/java/com/mrel/refreshcafe/SpecialsMenu.java
@@ -1,11 +1,13 @@
 package com.mrel.refreshcafe;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class SpecialsMenu extends AppCompatActivity {
 
@@ -25,6 +27,14 @@ public class SpecialsMenu extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Button specItem1 = (Button) findViewById(R.id.specItem1);
+        specItem1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SpecialsMenu.this, SpecialItm1.class));
+            }
+        });
     }
 
 }
